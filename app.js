62ea3806f7497bd28e4858fd14945985f44edded
Danish Mohammed <damo6513@colorado.edu>
2018-10-25 00:19:08 -0600

    Fixed bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const userRouter = require("./routes/users");
 app.get("/", (req, res) => res.send("Test \n"));
 app.use("/users", userRouter);
