5e1355f2157d47874f283901dc40f41b5d81b596
Danish Mohammed <damo6513@colorado.edu>
2018-10-25 18:21:01 -0600

    Allowing CORS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const userRouter = require("./routes/users");
+const cors = require("cors");
+app.use(cors());
 app.get("/", (req, res) => res.send("Test \n"));
 app.use("/users", userRouter);
 
