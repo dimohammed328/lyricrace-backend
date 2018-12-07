51e504da686b96a35aaad8107668273d2623494a
Danish Mohammed <dimohammed328@gmail.com>
2018-12-06 23:34:58 -0700

    bugfixes

--- routes/songselection.js
+++ routes/songselection.js
@@ -6,20 +6,14 @@ var { query } = require("../config/database.js");
 
 module.exports = function() {
   router.get("/", (req, res, next) => {
-    db.query(
-      "SELECT song,artist FROM songs",
-      [],
-      (err, result) => {
-        if (err) {
-          console.log("Error", err);
-          return next(err);
-        }
-        else{
-          res.send(results)
-        }
-
+    query("SELECT title,artist FROM songs", [], (err, result) => {
+      if (err) {
+        console.log("Error", err);
+        return next(err);
+      } else {
+        res.send(result);
       }
-    );
+    });
   });
   return router;
 };
